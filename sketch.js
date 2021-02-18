ca0193883721db8434a1c8780f6d4fa6cfb56cdf
Adiel1274 <78488019+Adiel1274@users.noreply.github.com>
2021-02-18 20:48:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var fixedrect,movingrect;
-
+var gameObject1,gameObject2,gameObject3,gameObject4;
 
 function setup() {
   createCanvas(800,400);
@@ -10,6 +10,15 @@ function setup() {
   movingrect=createSprite(300,200,50,30);
   movingrect.shapeColor="red";
   movingrect.debug=true;
+
+  gameObject1=createSprite(100,100,50,50);
+  gameObject1.shapeColor="green";
+  gameObject2=createSprite(200,100,50,50);
+  gameObject2.shapeColor="green";
+  gameObject3=createSprite(300,100,50,50);
+  gameObject3.shapeColor="green";
+  gameObject4=createSprite(400,100,50,50);
+  gameObject4.shapeColor="green";
 }
 
 function draw() {
@@ -18,14 +27,28 @@ function draw() {
 
   movingrect.x = World.mouseX;
   movingrect.y = World.mouseY;
-
-if (movingrect.x - fixedrect.x < fixedrect.width/2 + movingrect.width/2
-    && fixedrect.x - movingrect.x < fixedrect.width/2 + movingrect.width/2
-    && movingrect.y - fixedrect.y < fixedrect.height/2 + movingrect.height/2
-    && fixedrect.y - movingrect.y < fixedrect.height/2 + movingrect.height/2) {
-   movingrect.shapeColor = "blue"; 
-   fixedrect.shapeColor = "blue";
-    }
+  
+  if(isTouching(movingRect, gameObject1)){
+    movingRect.shapeColor = "blue";
+    gameObject1.shapeColor = "blue";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject1.shapeColor = "green";
+  }
+ 
 drawSprites();
 
+}
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
 }
